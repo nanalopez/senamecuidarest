9b0d6fade1ed2d3b1d3aa8c6a44fbd48bdde0f13
elidolosergio <agentcarvajal@gmail.com>
2020-10-29 16:54:05 -0500

    Habilitar usuarios 2

--- src/Routes/HabilitarUsuarios/habilitarRoutes.js
+++ src/Routes/HabilitarUsuarios/habilitarRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const HabilitarController = require('../../Controllers/HabilitarUsuarios/habilitar')
-const {userAuth, checkRole} = require('../../Utils/Auth');
+const {checkRole} = require('../../Utils/Auth');
 
 router.route("/listarsuspendidos").get(checkRole, HabilitarController.Listar_Suspendidos);
 router.route("/habilitarUsuario/:id").post(HabilitarController.Habilitar_Usuarios);
